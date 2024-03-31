7467569e04f6748a6cff4fd28f7e6eb1f8145b3f
Walid Kabboussa <walidkabou@gmail.com>
2024-03-31 02:50:46 +0000

    Setup transactions queries

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,5 +1,29 @@
-const transactionResolver =  {
-    Query: {},
+import Transaction from '../models/transaction.model'
+const transactionResolver = {
+    Query: {
+        transactions: async (_, _, context) => {
+            try {
+                if(!context.getUser()) throw new Error('Unauthorized')
+                const userId = await context.getUser()._id;
+
+                const transactions = await Transaction.find({ user: userId })
+                return transactions
+            } catch (error) {
+                console.error("Error in transaction query: ", error)
+                throw new Error(error.message || "Internal Server Error")
+            }
+        },
+        transaction: async(_, { transactionId },) => {
+            try {
+                const transaction = await Transaction.findById(transactionId)
+                return transaction
+                
+            } catch (error) {
+                console.error("Error in transaction query: ", error)
+                throw new Error(error.message || "Internal Server Error")
+            }
+        }
+    },
     Mutation: {}
 };
 
